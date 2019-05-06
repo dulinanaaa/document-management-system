bf80148eafc0d3223f1eedc887f891b3c1a39156
CatoyNan <xjn17858906105@gmail.com>
2019-05-06 23:24:41 +0800

    department

--- src/main/java/top/catoy/docmanagement/domain/Department.java
+++ src/main/java/top/catoy/docmanagement/domain/Department.java
@@ -1,45 +1,79 @@
 package top.catoy.docmanagement.domain;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @description: 部门
  * @author: xjn
  * @create: 2019-04-26 08:50
  **/
 public class Department {
-    private int departmentId;
-    private String departmentName;
-    private int superId;
+    private int id;
+    private String name;
+    private int parent_id;
+    private String instroduction;
 
-    public int getDepartmentId() {
-        return departmentId;
+    private int sort;
+    private List<Department> children;
+
+    public int getId() {
+        return id;
     }
 
-    public void setDepartmentId(int departmentId) {
-        this.departmentId = departmentId;
+    public void setId(int id) {
+        this.id = id;
     }
 
-    public String getDepartmentName() {
-        return departmentName;
+    public String getName() {
+        return name;
     }
 
-    public void setDepartmentName(String departmentName) {
-        this.departmentName = departmentName;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public int getSuperId() {
-        return superId;
+    public int getParent_id() {
+        return parent_id;
     }
 
-    public void setSuperId(int superId) {
-        this.superId = superId;
+    public void setParent_id(int parent_id) {
+        this.parent_id = parent_id;
+    }
+
+    public String getInstroduction() {
+        return instroduction;
+    }
+
+    public void setInstroduction(String instroduction) {
+        this.instroduction = instroduction;
+    }
+
+    public int getSort() {
+        return sort;
+    }
+
+    public void setSort(int sort) {
+        this.sort = sort;
+    }
+
+    public List<Department> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<Department> children) {
+        this.children = children;
     }
 
     @Override
     public String toString() {
         return "Department{" +
-                "departmentId=" + departmentId +
-                ", departmentName='" + departmentName + '\'' +
-                ", superId=" + superId +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", parent_id=" + parent_id +
+                ", instroduction='" + instroduction + '\'' +
+                ", sort=" + sort +
+                ", children=" + children +
                 '}';
     }
 }
